fae2a945d99eaba3f49871ca059d99c000195c19
oscar <oscartrent6@gmail.com>
2025-03-08 19:30:18 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+function setBestCustomer(customerName){
+    return bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    if (bestCustomer= 'not bob') {
+        return bestCustomer = 'maybe bob'
+    }
+}
+
+const leastFavoriteCustomer = 'jacob'
+
+function changeLeastFavoriteCustomer() {
+    if (leastFavoriteCustomer = 'jacob'){
+        return leastFavoriteCustomer = 'bev'
+    }
+}
